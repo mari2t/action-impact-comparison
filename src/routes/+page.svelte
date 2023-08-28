dbfc89213dcf5b99797954c05d7e393081bd03c5
mari2t <subcodeoft@gmail.com>
2023-08-28 17:16:53 +0900

    style : 外観

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,87 @@
-<!-- src/App.svelte -->
 <script>
-  let doMerits = "";
-  let doDemerits = "";
-  let notDoMerits = "";
-  let notDoDemerits = "";
+  let worryText = "";
+  let inactionText = "";
+  let actionText = "";
+
+  let worryOutput = "";
+  let inactionOutput = "";
+  let actionOutput = "";
+
+  function updateWorry() {
+    worryOutput = worryText;
+    worryText = "";
+  }
+
+  function updateInaction() {
+    inactionOutput = inactionText;
+    inactionText = "";
+  }
+
+  function updateAction() {
+    actionOutput = actionText;
+    actionText = "";
+  }
 </script>
 
-<div class="container">
-  <div class="grid">
-    <div>
-      <h2 class="text-xl font-semibold">行う場合</h2>
-      <label for="doMerits" class="block mt-4">メリット</label>
-      <textarea
-        id="doMerits"
-        bind:value={doMerits}
-        class="w-full h-24 p-2 border rounded"
-      />
+<div class="text-center">
+  <!-- 悩んでいること -->
+  <div class="m-4">
+    <label for="worry" class="mr-2">悩んでいること: </label>
+    <input id="worry" type="text" class="border p-2" bind:value={worryText} />
+    <button class="bg-gray-500 text-white p-2 ml-2" on:click={updateWorry}
+      >OK</button
+    >
+    <div>{worryOutput}</div>
+  </div>
+</div>
 
-      <label for="doDemerits" class="block mt-4">デメリット</label>
-      <textarea
-        id="doDemerits"
-        bind:value={doDemerits}
-        class="w-full h-24 p-2 border rounded"
-      />
-    </div>
-
-    <div>
-      <h2 class="text-xl font-semibold">行わない場合</h2>
-      <label for="notDoMerits" class="block mt-4">メリット</label>
-      <textarea
-        id="notDoMerits"
-        bind:value={notDoMerits}
-        class="w-full h-24 p-2 border rounded"
-      />
-
-      <label for="notDoDemerits" class="block mt-4">デメリット</label>
-      <textarea
-        id="notDoDemerits"
-        bind:value={notDoDemerits}
-        class="w-full h-24 p-2 border rounded"
-      />
+<div class="flex h-screen">
+  <div class="flex flex-col w-1/2 bg-blue-100">
+    <!-- 行動しない場合 -->
+    <div class="m-4">
+      <label for="inaction" class="mr-2">行動しない場合: </label>
+      <div class="flex">
+        <img
+          src="/src/lib/images/inaction-icon.png"
+          alt="Inaction scenario"
+          class="w-12 h-12"
+        />
+        <input
+          id="inaction"
+          type="text"
+          class="border p-2"
+          bind:value={inactionText}
+        />
+        <button
+          class="bg-blue-500 text-white p-2 ml-2"
+          on:click={updateInaction}>OK</button
+        >
+      </div>
+      <div>{inactionOutput}</div>
     </div>
   </div>
+  <div class="flex flex-col w-1/2 bg-[#f8e8ee]">
+    <!-- 行動する場合 -->
 
-  <div class="mt-8">
-    <h2 class="text-xl font-semibold">比較結果</h2>
-    <p>行う場合のメリット: {doMerits}</p>
-    <p>行う場合のデメリット: {doDemerits}</p>
-    <p>行わない場合のメリット: {notDoMerits}</p>
-    <p>行わない場合のデメリット: {notDoDemerits}</p>
+    <div class="m-4">
+      <label for="action" class="mr-2 text-center">行動する場合: </label>
+      <div class="flex">
+        <img
+          src="/src/lib/images/action-icon.png"
+          alt="Action scenario"
+          class="w-12 h-12"
+        />
+        <input
+          id="action"
+          type="text"
+          class="border p-2"
+          bind:value={actionText}
+        />
+        <button class="bg-pink-500 text-white p-2 ml-2" on:click={updateAction}
+          >OK</button
+        >
+      </div>
+      <div>{actionOutput}</div>
+    </div>
   </div>
 </div>
